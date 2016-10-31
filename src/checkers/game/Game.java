aaeddb7f296189e11f1aed1ee9d598302a05fbf9
Ariel <arielhr1987@gmail.com>
2016-10-30 23:20:31 -0800

    creating game class

--- src/checkers/game/Game.java
+++ src/checkers/game/Game.java
@@ -1,10 +1,36 @@
 package checkers.game;
 
+import checkers.ai.Brain;
+
 /**
  * Created by Ariel on 10/28/2016.
  */
 public class Game {
 
     private Board board;
+
+    private Brain brain;
+
+    public Game() {
+
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public void setBoard(Board board) {
+        this.board = board;
+    }
+
+    public Brain getBrain() {
+        return brain;
+    }
+
+    public void setBrain(Brain brain) {
+        this.brain = brain;
+    }
+
+
 
 }
